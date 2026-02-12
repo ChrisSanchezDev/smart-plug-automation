dcd59f7bc4a38eeeecfa671fd9b2d4e39de02a46
ChrisSanchezDev <ChrisSanchezDev@gmail.com>
2026-02-11 20:36:50 -0500

    feat: logger.py works for both linux and windows now (should)

--- logger.py
+++ logger.py
@@ -1,28 +1,38 @@
 import logging
 import os
+import sys
+from pathlib import Path
 from dotenv import load_dotenv
 from logging.handlers import RotatingFileHandler
 
-load_dotenv()
+# Path(__file__) grabs the relative path of the curr file
+# .resolve(): opens that up to the absolute path
+# .parent: only gets the parent path rather than the specific item itself.
+BASE_DIR = Path(__file__).resolve().parent
+
+load_dotenv(BASE_DIR / '.env')
 
 # ----- Adjust this section per project -----
-RELATIVE_LOG_FILE = 'scripts/smart-plug-automation/logs/smart-plug-automation.log'
+LOG_FOLDER_NAME = 'logs'
+LOG_FILE_NAME = 'smart-plug-automation.log'
 LOGGER_NAME = 'smart-plug-automation-logger'
 # -------------------------------------------
 
-home_dir = os.path.expanduser('~')
-LOG_FILE = os.path.join(home_dir, RELATIVE_LOG_FILE)
+LOG_DIR = BASE_DIR / LOG_FOLDER_NAME # C:\Users\chris\TP_CodingFolder\03_PersonalLab\00_HomeLab\smart-plug-automation + \logs
+LOG_FILE_PATH = LOG_DIR / LOG_FILE_NAME # C:\Users\chris\TP_CodingFolder\03_PersonalLab\00_HomeLab\smart-plug-automation\logs + \smart-plug-automation.log
 
-os.makedirs(os.path.dirname(LOG_FILE), exist_ok=True)
+# Create log folder directory if it doesn't exist already.
+# parents=True makes parent folders if necessary (logs)
+LOG_DIR.mkdir(parents=True, exists_ok=True)
 
-LOG_STATE = os.getenv('LOG_STATE')
-LOG_ONLY = bool(os.getenv('LOG_ONLY'))
+LOG_STATE = os.getenv('LOG_STATE', 'info')
+LOG_ONLY = str(os.getenv('LOG_ONLY', 'False')).lower() in ('true', '1', 't') # Handles multiple version of True. Is still a boolean
 
 MAX_BYTES = 5 * 1024 * 1024 # 5MB
 BACKUP_COUNT = 3
 
 rotate_handler = RotatingFileHandler(
-    LOG_FILE,
+    LOG_FILE_PATH,
     maxBytes=MAX_BYTES,
     backupCount=BACKUP_COUNT
 )
@@ -40,6 +50,6 @@ else:
 logger.addHandler(rotate_handler)
 
 if not LOG_ONLY:
-    console_handler = logging.StreamHandler()
+    console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
